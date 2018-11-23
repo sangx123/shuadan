4d22cd04d941d3777e96090b6985f0e9b477d204
桑享 <sangx@emucoo.com>
2018-11-23 16:14:46 +0800

    重新创建

--- dao/src/main/java/com/sangxiang/dao/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sangxiang.dao.mapper;
-
-
-import com.sangxiang.dao.model.User;
-
-public interface UserMapper {
-    int insert(User record);
-    User getUser();
-}
--- dao/src/main/java/com/sangxiang/dao/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.sangxiang.dao.service;
-
-import com.sangxiang.dao.model.User;
-
-public interface UserService {
-    int insert(User record);
-    User getUser();
-}
